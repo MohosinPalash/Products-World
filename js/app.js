bc587643e202b6b9ea76bebe8357a099d8793801
Mohosin Islam Palash <mohosinislampolash@gmail.com>
2021-09-13 13:51:39 +0600

    Second Commit

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p>No. of Ratings: ${product.rating.count}</p>
+      <p>Average Rating: ${product.rating.rate}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -28,15 +30,16 @@ const showProducts = (products) => {
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
-  updatePrice("price", price);
-
-  updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updatePrice("price", price);
+  updateTaxAndCharge();
+  updateTotal();
+
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -45,7 +48,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -75,6 +78,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
+  console.log(grandTotal);
   document.getElementById("total").innerText = grandTotal;
 };
 loadProducts();
